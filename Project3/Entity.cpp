6e576d04b2c6212ec2abe2cca44a172d66725d5d
jw7914 <jw7914@nyu.edu>
2024-10-24 14:49:05 -0400

    adding additional methods for static non-animation sprites

--- Project3/Entity.cpp
+++ Project3/Entity.cpp
@@ -18,24 +18,11 @@ Entity::Entity()
     : m_position(0.0f), m_movement(0.0f), m_scale(1.0f, 1.0f, 0.0f), m_model_matrix(1.0f),
       m_speed(0.0f), m_animation_cols(0), m_animation_frames(0), m_animation_index(0),
       m_animation_rows(0), m_animation_indices(nullptr), m_animation_time(0.0f),
-      m_texture_id(0)
+      m_texture_id(0), m_velocity(0.0f), m_acceleration(0.0f)
 {
     // Initialize m_walking with zeros or any default value
     for (int i = 0; i < SECONDS_PER_FRAME; ++i)
         for (int j = 0; j < SECONDS_PER_FRAME; ++j) m_walking[i][j] = 0;
-}
-
-// Parameterized constructor
-Entity::Entity(GLuint texture_id, float speed, int walking[4][4], float animation_time,
-               int animation_frames, int animation_index, int animation_cols,
-               int animation_rows)
-    : m_position(0.0f), m_movement(0.0f), m_scale(1.0f, 1.0f, 0.0f), m_model_matrix(1.0f),
-      m_speed(speed), m_animation_cols(animation_cols),
-      m_animation_frames(animation_frames), m_animation_index(animation_index),
-      m_animation_rows(animation_rows), m_animation_indices(nullptr),
-      m_animation_time(animation_time), m_texture_id(texture_id)
-{
-    set_walking(walking);
 }
 
 // Simpler constructor for partial initialization
@@ -43,7 +30,19 @@ Entity::Entity(GLuint texture_id, float speed)
     : m_position(0.0f), m_movement(0.0f), m_scale(1.0f, 1.0f, 0.0f), m_model_matrix(1.0f),
       m_speed(speed), m_animation_cols(0), m_animation_frames(0), m_animation_index(0),
       m_animation_rows(0), m_animation_indices(nullptr), m_animation_time(0.0f),
-      m_texture_id(texture_id)
+      m_texture_id(texture_id), m_velocity(0.0f), m_acceleration(0.0f)
+{
+    // Initialize m_walking with zeros or any default value
+    for (int i = 0; i < SECONDS_PER_FRAME; ++i)
+        for (int j = 0; j < SECONDS_PER_FRAME; ++j) m_walking[i][j] = 0;
+}
+
+//Constructor for no animation with spritesheet
+Entity::Entity(GLuint texture_id, float speed, int index, int cols, int rows)
+    : m_position(0.0f), m_movement(0.0f), m_scale(1.0f, 1.0f, 0.0f), m_model_matrix(1.0f),
+      m_speed(speed), m_animation_cols(cols), m_animation_frames(0), m_animation_index(index),
+      m_animation_rows(rows), m_animation_indices(nullptr), m_animation_time(0.0f),
+      m_texture_id(texture_id), m_velocity(0.0f), m_acceleration(0.0f), m_rotate_state(1)
 {
     // Initialize m_walking with zeros or any default value
     for (int i = 0; i < SECONDS_PER_FRAME; ++i)
@@ -90,8 +89,56 @@ void Entity::draw_sprite_from_texture_atlas(ShaderProgram *program, GLuint textu
     glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
 }
 
-void Entity::update(float delta_time)
+void Entity::face_up() {
+    if (m_animation_indices != NULL) {
+        m_animation_indices = m_walking[UP];
+    } else {
+        // Rotate 90 degrees counterclockwise to face up
+        m_rotate_state = 1;
+    }
+}
+
+void Entity::face_down() {
+    if (m_animation_indices != NULL) {
+        m_animation_indices = m_walking[DOWN];
+    } else {
+        // Rotate 90 degrees clockwise to face down
+        m_rotate_state = 2;
+    }
+}
+
+void Entity::face_left() {
+    if (m_animation_indices != NULL) {
+        m_animation_indices = m_walking[LEFT];
+    } else {
+        // Rotate 180 degrees to face left
+        m_rotate_state = 3;
+    }
+}
+
+void Entity::face_right() {
+    if (m_animation_indices != NULL) {
+        m_animation_indices = m_walking[RIGHT];
+    } else {
+        m_rotate_state =4;
+    }
+}
+
+
+bool const Entity::check_collision(Entity* other) const
 {
+    float x_distance = fabs(m_position.x - other->m_position.x) - ((m_width + other->m_width) / 2.0f);
+    float y_distance = fabs(m_position.y - other->m_position.y) - ((m_height + other->m_height) / 2.0f);
+
+    return x_distance < 0.0f && y_distance < 0.0f;
+}
+void Entity::update(float delta_time, Entity* collidable_entities, int collidable_entity_count)
+{
+    for (int i = 0; i < collidable_entity_count; i++)
+    {
+        if (check_collision(&collidable_entities[i])) return;
+    }
+
     if (m_animation_indices != NULL)
     {
         if (glm::length(m_movement) != 0)
@@ -112,9 +159,24 @@ void Entity::update(float delta_time)
         }
     }
     
-    m_position += m_movement * m_speed * delta_time;
+    // Our character moves from left to right, so they need an initial velocity
+    m_velocity.x = m_movement.x * m_speed;
+    
+    // And we add the gravity next
+    m_velocity += m_acceleration * delta_time;
+    m_position += m_velocity * delta_time;
+    
     m_model_matrix = glm::mat4(1.0f);
     m_model_matrix = glm::translate(m_model_matrix, m_position);
+    if (m_rotate_state == 1) { // UP
+        m_model_matrix = glm::rotate(m_model_matrix, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    else if(m_rotate_state == 2) { // Down
+        m_model_matrix = glm::rotate(m_model_matrix, glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    else if (m_rotate_state == 3) { // Left
+        m_model_matrix = glm::rotate(m_model_matrix, glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    }
     m_model_matrix = glm::scale(m_model_matrix, m_scale);
 }
 
@@ -122,10 +184,13 @@ void Entity::render(ShaderProgram *program)
 {
     program->set_model_matrix(m_model_matrix);
     
+    if (m_animation_indices == NULL && m_animation_cols != 0 && m_animation_rows != 0) {
+        draw_sprite_from_texture_atlas(program, m_texture_id, m_animation_index);
+        return;
+    }
     if (m_animation_indices != NULL)
     {
-        draw_sprite_from_texture_atlas(program, m_texture_id,
-                                       m_animation_indices[m_animation_index]);
+        draw_sprite_from_texture_atlas(program, m_texture_id, m_animation_indices[m_animation_index]);
         return;
     }
     
--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,10 +8,10 @@
 * Academic Misconduct.
 **/
 
-#define GL_SILENCE_DEPRECATION
-#define STB_IMAGE_IMPLEMENTATION
-#define GL_GLEXT_PROTOTYPES 1
 #define LOG(argument) std::cout << argument << '\n'
+#define STB_IMAGE_IMPLEMENTATION
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
 
 #ifdef _WINDOWS
     #include <GL/glew.h>
@@ -23,55 +23,66 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 #include "stb_image.h"
-#include "Entity.h"
-#include <vector>
-#include <ctime>
 #include "cmath"
+#include <ctime>
+#include <vector>
+#include "Entity.h"
 
-enum AppStatus { RUNNING, TERMINATED };
+// ————— CONSTANTS ————— //
+constexpr int WINDOW_WIDTH  = 640 * 2,
+              WINDOW_HEIGHT = 800;
 
-constexpr int WINDOW_WIDTH  = 640,
-              WINDOW_HEIGHT = 480;
-
-constexpr float BG_RED     = 0.9765625f,
-                BG_GREEN   = 0.97265625f,
-                BG_BLUE    = 0.9609375f,
+constexpr float BG_RED = 0.1922f,
+                BG_BLUE = 0.549f,
+                BG_GREEN = 0.9059f,
                 BG_OPACITY = 1.0f;
 
-constexpr int VIEWPORT_X      = 0,
-              VIEWPORT_Y      = 0,
+constexpr int VIEWPORT_X = 0,
+              VIEWPORT_Y = 0,
               VIEWPORT_WIDTH  = WINDOW_WIDTH,
               VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
 constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
                F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
-constexpr float MILLISECONDS_IN_SECOND = 1000.0f;
+constexpr float MILLISECONDS_IN_SECOND = 1000.0;
+constexpr char  SPRITESHEET_FILEPATH[] = "george_0.png",
+                PLATFORM_FILEPATH[]    = "platformPack_tile027.png",
+                SPACESHIP_FILEPATH[]   = "Spaceships.png";
 
-constexpr GLint NUMBER_OF_TEXTURES = 1,
-                LEVEL_OF_DETAIL    = 0,
-                TEXTURE_BORDER     = 0;
+constexpr GLint NUMBER_OF_TEXTURES = 1;
+constexpr GLint LEVEL_OF_DETAIL    = 0;
+constexpr GLint TEXTURE_BORDER     = 0;
 
-constexpr char SHIELD_SPRITE_FILEPATH[] = "shield.png";
-constexpr glm::vec3 INIT_SCALE = glm::vec3(2.0f, 2.3985f, 0.0f);
+constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
+constexpr float ACC_OF_GRAVITY = -9.81f;
+constexpr int   PLATFORM_COUNT = 5;
 
-SDL_Window* g_display_window = nullptr;
+// ————— STRUCTS AND ENUMS —————//
+enum AppStatus { RUNNING, TERMINATED };
+
+struct GameState
+{
+    Entity* player;
+    Entity* platforms;
+};
+
+// ————— VARIABLES ————— //
+GameState g_game_state;
+
+SDL_Window* g_display_window;
 AppStatus g_app_status = RUNNING;
 
 ShaderProgram g_shader_program = ShaderProgram();
+glm::mat4 g_view_matrix, g_projection_matrix;
 
-GLuint g_shield_texture_id;
+float g_previous_ticks   = 0.0f;
+float g_time_accumulator = 0.0f;
 
-glm::mat4 g_view_matrix,
-          g_shield_matrix,
-          g_projection_matrix;
-
-float g_previous_ticks = 0.0f;
-
-glm::vec3 g_shield_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 g_shield_movement = glm::vec3(0.0f, 0.0f, 0.0f);
-
-float g_shield_speed = 5.0f;  // move 1 unit per second
+// ———— GENERAL FUNCTIONS ———— //
+GLuint load_texture(const char* filepath);
+void draw_sprite_from_texture_atlas(ShaderProgram *program, GLuint texture_id, int index,
+                                    int rows, int cols);
 
 void initialise();
 void process_input();
@@ -79,13 +90,8 @@ void update();
 void render();
 void shutdown();
 
-GLuint load_texture(const char* filepath);
-void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id);
-
-
 GLuint load_texture(const char* filepath)
 {
-    // STEP 1: Loading the image file
     int width, height, number_of_components;
     unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components,
                                      STBI_rgb_alpha);
@@ -96,37 +102,77 @@ GLuint load_texture(const char* filepath)
         assert(false);
     }
 
-    // STEP 2: Generating and binding a texture ID to our image
     GLuint textureID;
     glGenTextures(NUMBER_OF_TEXTURES, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
     glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER,
                  GL_RGBA, GL_UNSIGNED_BYTE, image);
 
-    // STEP 3: Setting our texture filter parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    // STEP 4: Releasing our file from memory and returning our texture id
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
     stbi_image_free(image);
 
     return textureID;
 }
 
+void draw_sprite_from_texture_atlas(ShaderProgram *shaderProgram, GLuint texture_id, int index, int rows, int cols)
+{
+    // Step 1: Calculate the UV location of the indexed frame
+    float u_coord = (float) (index % cols) / (float) cols;
+    float v_coord = (float) (index / cols) / (float) rows;
+
+    // Step 2: Calculate its UV size
+    float width = 1.0f / (float) cols;
+    float height = 1.0f / (float) rows;
+
+    // Step 3: Just as we have done before, match the texture coordinates to the vertices
+    float tex_coords[] =
+    {
+        u_coord, v_coord + height, u_coord + width, v_coord + height, u_coord + width,
+        v_coord, u_coord, v_coord + height, u_coord + width, v_coord, u_coord, v_coord
+    };
+
+    float vertices[] =
+    {
+        -0.5, -0.5, 0.5, -0.5,  0.5, 0.5,
+        -0.5, -0.5, 0.5,  0.5, -0.5, 0.5
+    };
+
+    // Step 4: And render
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glVertexAttribPointer(shaderProgram->get_position_attribute(), 2, GL_FLOAT, false, 0,
+                          vertices);
+    glEnableVertexAttribArray(shaderProgram->get_position_attribute());
+
+    glVertexAttribPointer(shaderProgram->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0,
+                          tex_coords);
+    glEnableVertexAttribArray(shaderProgram->get_tex_coordinate_attribute());
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(shaderProgram->get_position_attribute());
+    glDisableVertexAttribArray(shaderProgram->get_tex_coordinate_attribute());
+}
 
 void initialise()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Project 3",
-                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                      WINDOW_WIDTH, WINDOW_HEIGHT,
-                                      SDL_WINDOW_OPENGL);
+    g_display_window = SDL_CreateWindow("Hello, Entities!",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
 
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
-
-    if (g_display_window == nullptr)
+    
+    if (context == nullptr)
     {
+        LOG("ERROR: Could not create OpenGL context.\n");
         shutdown();
     }
 
@@ -138,8 +184,6 @@ void initialise()
 
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
-    g_shield_texture_id = load_texture(SHIELD_SPRITE_FILEPATH);
-    g_shield_matrix     = glm::mat4(1.0f);
     g_view_matrix       = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
@@ -147,87 +191,138 @@ void initialise()
     g_shader_program.set_view_matrix(g_view_matrix);
 
     glUseProgram(g_shader_program.get_program_id());
+
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
 
+    // ————— PLAYER ————— //
+    GLuint player_texture_id = load_texture(SPACESHIP_FILEPATH);
+    
+
+    g_game_state.player = new Entity(
+        player_texture_id,         // texture id
+        1.0f,                      // speed
+        9,                         // current animation index
+        5,                         // animation column amount
+        3                          // animation row amount
+    );
+
+    g_game_state.player->face_down();
+    g_game_state.player->set_position(glm::vec3(4.0f,2.75f,0.0f));
+//    g_game_state.player->set_acceleration(glm::vec3(0.0f, ACC_OF_GRAVITY * 0.1, 0.0f));
+
+    // ————— PLATFORM ————— //
+    g_game_state.platforms = new Entity[PLATFORM_COUNT];
+
+    for (int i = 0; i < PLATFORM_COUNT; i++)
+    {
+        g_game_state.platforms[i].set_texture_id(load_texture(PLATFORM_FILEPATH));
+        g_game_state.platforms[i].set_position(glm::vec3(i - 1.0f, -3.0f, 0.0f));
+        g_game_state.platforms[i].update(0.0f, nullptr, 0);
+    }
+
+    // ————— GENERAL ————— //
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-
 void process_input()
 {
+    // VERY IMPORTANT: If nothing is pressed, we don't want to go anywhere
+    g_game_state.player->set_movement(glm::vec3(0.0f));
+
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
-        {
+        switch (event.type) {
+            // End game
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
             g_app_status = TERMINATED;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_q:
+                // Quit the game with a keystroke
+                g_app_status = TERMINATED;
+                break;
+
+            default:
+                break;
+            }
+
+        default:
+            break;
         }
     }
-}
 
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_LEFT])       g_game_state.player->move_left();
+    else if (key_state[SDL_SCANCODE_RIGHT]) g_game_state.player->move_right();
+    
+    if(key_state[SDL_SCANCODE_UP]) g_game_state.player->move_up();
+    else if (key_state[SDL_SCANCODE_DOWN]) g_game_state.player->move_down();
+
+    // This makes sure that the player can't move faster diagonally
+    if (glm::length(g_game_state.player->get_movement()) > 1.0f)
+        g_game_state.player->normalise_movement();
+}
 
 void update()
 {
-    /* DELTA TIME */
-    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    // ————— DELTA TIME ————— //
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
 
-    /* GAME LOGIC */
-    g_shield_position += g_shield_movement * g_shield_speed * delta_time;
+    // ————— FIXED TIMESTEP ————— //
+    // STEP 1: Keep track of how much time has passed since last step
+    delta_time += g_time_accumulator;
 
-    /* TRANSFORMATIONS */
-    g_shield_matrix = glm::mat4(1.0f);
+    // STEP 2: Accumulate the ammount of time passed while we're under our fixed timestep
+    if (delta_time < FIXED_TIMESTEP)
+    {
+        g_time_accumulator = delta_time;
+        return;
+    }
 
-    g_shield_matrix = glm::translate(g_shield_matrix, g_shield_position);
-    g_shield_matrix = glm::scale(g_shield_matrix, INIT_SCALE);
+    // STEP 3: Once we exceed our fixed timestep, apply that elapsed time into the
+    //         objects' update function invocation
+    while (delta_time >= FIXED_TIMESTEP)
+    {
+        // Notice that we're using FIXED_TIMESTEP as our delta time
+        g_game_state.player->update(FIXED_TIMESTEP, g_game_state.platforms,
+                                    PLATFORM_COUNT);
+        delta_time -= FIXED_TIMESTEP;
+    }
+
+    g_time_accumulator = delta_time;
 }
-
-
-void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
-{
-    g_shader_program.set_model_matrix(object_model_matrix);
-    glBindTexture(GL_TEXTURE_2D, object_texture_id);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-}
-
 
 void render()
 {
+    // ————— GENERAL ————— //
     glClear(GL_COLOR_BUFFER_BIT);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
-        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
-    };
+    // ————— PLAYER ————— //
+    g_game_state.player->render(&g_shader_program);
 
-    // Textures
-    float texture_coordinates[] = {
-        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
-        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
-    };
+    // ————— PLATFORM ————— //
+    for (int i = 0; i < PLATFORM_COUNT; i++)
+        g_game_state.platforms[i].render(&g_shader_program);
 
-    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
-                          0, vertices);
-    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
-
-    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
-                          false, 0, texture_coordinates);
-    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
-
-    // Bind texture
-    draw_object(g_shield_matrix, g_shield_texture_id);
-
-    // We disable two attribute arrays now
-    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
-    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
-
+    // ————— GENERAL ————— //
     SDL_GL_SwapWindow(g_display_window);
 }
 
-
-void shutdown() { SDL_Quit(); }
+void shutdown()
+{
+    SDL_Quit();
+    
+    delete   g_game_state.player;
+    delete[] g_game_state.platforms;
+}
 
 
 int main(int argc, char* argv[])
